1edf86cf66cacf8205690846c6d7fd910016e7eb
Yonni Chen <yonni.scholars@gmail.com>
2018-01-29 12:48:37 -0500

    Added geolocation support in client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,28 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { SuggestionService } from './suggestion.service';
 import { City } from './../../../common/models/city';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  
-  private searchTerm: string;
-  private cities: City[];
-  
-  constructor(private suggestionService_: SuggestionService) {
-    this.searchTerm = "";
-  }
-
-  ngOnInit(): void {
-    this.suggestionService_.getSuggestions(this.searchTerm).then((result) => {
-      this.cities = result;
-    });
-  }
-
-  searchTermChanged(event: any): void {
-    console.log(this.searchTerm);
-  }
+export class AppComponent {
+    
+    private searchTerm: string;
+    private isLocationActivated: boolean;
+    private cities: City[];
+    private latitude?: number;
+    private longitude?: number;
+    
+    constructor(private suggestionService_: SuggestionService) {
+        this.searchTerm = "";
+        this.isLocationActivated = false;
+    }
+    
+    public activateLocation(event: Event) {
+        if (this.isLocationActivated) {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition((position) => {
+                    this.latitude = position.coords.latitude;
+                    this.longitude = position.coords.longitude;
+                    this.searchTermChanged();
+                });
+            } else {
+                alert("Geolocation is not supported by this browser.");
+                this.latitude = undefined;
+                this.longitude = undefined;
+            }
+        }
+        else {
+            this.latitude = undefined;
+            this.longitude = undefined;
+            this.searchTermChanged();
+        }
+    }
+    
+    public searchTermChanged(event?: Event): void {
+        // Make sure that user finished to type. We don't want to spam the server for every key stroke...
+        setTimeout(() => {
+            this.suggestionService_.getSuggestions(this.searchTerm, this.latitude, this.longitude).then((result) => {
+                this.cities = result;
+            });
+        }, 400);
+    }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SuggestionService } from './suggestion.service';
@@ -9,18 +10,21 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpModule,
     MatInputModule,
     MatCardModule,
-    MatGridListModule
+    MatGridListModule,
+    MatCheckboxModule
   ],
   providers: [
     SuggestionService
