e2462cd45b8ae84bebec32eef7962c2f52213010
Yonni Chen <yonni.scholars@gmail.com>
2018-01-29 00:23:24 -0500

    Introduce a SuggestionService for the client

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SuggestionService } from './suggestion.service';
 
 
 @NgModule({
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    SuggestionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/src/app/suggestion.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions, URLSearchParams } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { City } from './../../../common/models/city';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class SuggestionService {
+
+    private baseUrl = "http://localhost:3000/api/suggestions";
+
+    constructor(private http: Http) {
+
+    }
+
+    public getSuggestions(searchTerm: string, latitude ?: number, longitude ?: number): Promise<City[]> {
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let params = new URLSearchParams();
+        params.set('q', searchTerm);
+
+        if (latitude !== undefined && longitude !== undefined) {
+            params.set('latitude', latitude.toString());
+            params.set('longitude', longitude.toString());
+        }
+
+        let options = new RequestOptions({ 
+            headers: headers,
+            params: params
+        });
+
+        return this.http.get(this.baseUrl, options)
+            .toPromise()
+            .then((res) => {
+                return res.json();
+            });
+    }
+}
